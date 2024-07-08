b9519a86c75abb827baab49c7099f0453b98a0a0
Gilang Aditia <gaditia744@gmail.com>
2024-07-08 10:49:41 +0700

    update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -31,6 +31,35 @@ export default function Home() {
           </p>
         </Card>
       </div>
+      <div className="grid gap-4 p-3 sm:grid-cols-2">
+        <Card
+          href="https://gilang-aditia.github.io/PT_Boga/"
+          className="max-w-sm">
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Company Profile PT BOGA ETERNA SENTOSA
+          </h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            PT. Boga Eterna Sentosa adalah perusahaan importir makanan yang
+            berlokasi di Indonesia, dengan fokus pada produk makanan Korea,
+            Cina, dan Jepang yang telah tersertifikasi halal. PT. Boga Eterna
+            Sentosa berkomitmen untuk menyediakan pilihan makanan berkualitas
+            tinggi yang memenuhi standar kehalalan, sehingga Anda dapat
+            menikmati kelezatan autentik tanpa kekhawatiran.
+          </p>
+        </Card>
+        <Card href="https://react-chill-movie.web.app/" className="max-w-sm">
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Movie Chill
+          </h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Movie Chill adalah sebuah situs web inovatif yang dirancang untuk
+            para pecinta film untuk menemukan, menonton, dan melacak film
+            favorit mereka. Dengan perpustakaan film yang luas dari berbagai
+            genre dan era, Movie Chill menyediakan pengalaman streaming yang
+            mulus dan disesuaikan dengan preferensi setiap pengguna.
+          </p>
+        </Card>
+      </div>
     </>
   );
 }
